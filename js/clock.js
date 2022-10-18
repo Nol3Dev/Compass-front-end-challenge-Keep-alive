251219f7a044decd3585ddbd104e474a71e583c4
Nol3Dev <allanb2805@gmail.com>
2022-10-18 14:27:40 -0300

    Home Clock & Date complete

--- js/clock.js
+++ js/clock.js
@@ -1,113 +1,26 @@
-let clock = document.querySelector('#clock');
-let date = document.querySelector('#date');
-let hour = document.querySelector('#hour');
-let minute = document.querySelector('#minute');
-
-let timeDate = new Date();
-
-function clockAnimation() {
-
-    let nowIs = new Date();
-
-    let hour = nowIs.getHours();
-    let minute = nowIs.getMinutes();
-
-    let strhour = new String(hour);
-    let strminute = new String(minute);
-
-    if(strminute.length == 1){
-        minute = "0" + minute;
-    }
-
-    if(strhour.length == 1){
-        hour = "0" + hour;
-    }
-
-    h.textContent = hour;   
-    m.textContent = minute;
-
-    setTimeout("clockAnimation()",1000);
-
+updateTime = () => {
+    var current = new Date();
+    var nowHour = numberHour(current.getHours()) + ':' + numberHour(current.getMinutes());
+    const hour =  document.querySelector(".time__hour");
+    hour.innerHTML = nowHour
 }
 
-function getDate() {
-
-    let day = timeDate.getDate();
-    let month = timeDate.getMonth()+1;
-    let year = timeDate.getFullYear();
-
-    let strday = new String(day);
-    let strmonth = new String(month);
-
-    if(strday.length == 1){
-        day = "0" + day;
+function numberHour(n){
+    if(n < 10){
+        n = "0" + n;
     }
-
-    if(strmonth.length == 1){
-        month = "0" + month;
-    }
-
-    switch(month) {
-        case 01:
-            month = "Janeiro";
-        break;    
-    
-        case 02:
-            month = "Fevereiro";
-        break;
-
-        case 03:
-            month = "Março";
-        break;
-
-        case 04:
-            month = "Abril";
-        break;
-
-        case 05:
-            month = "Maio";
-    
-        case 06:
-            month = "Junho";
-        break;
-
-        case 07:
-            month = "Julho";
-        break;
-
-        case 08:
-            month = "Agosto";
-        break;
-
-        case 09:
-            month = "Setembro";
-        break;
-
-        case 10:
-            month = "Outubro";
-        break;
-
-        case 11:
-            month = "Novembro";
-        break;
-
-        default:
-            month = "Dezembro";
-        
-    }
-    
-    let dataAtual = day + ' de ' + month + ' de ' + year;
-
-    data.textContent = dataAtual;
-
+    return n;
 }
 
-getDate();
+updateTime();
+setInterval(updateTime, 10000);
 
-var options = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric'
+function createDate(){
+    const date = document.querySelector(".time__date");
+    var monthName = new Array ("janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro");
+    var weekDay = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"];
+    var currentDate = new Date;
+    date.innerHTML = (`${weekDay[currentDate.getDay()]}, ${currentDate.getDate()} de ${monthName[currentDate.getMonth()]} de ${currentDate.getFullYear()}`);
 }
 
-let test = new Date();
+createDate();
